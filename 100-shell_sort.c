f49c795ff1403f8895b48e12d86ab34395374f3b
Popsicool <akinolasamson1234@gmail.com>
2022-11-08 06:23:07 +0000

    task 3

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,5 +1,13 @@
 #include "sort.h"
 
+
+/**
+ * shell_sort -  sorts an array of integers in ascending order
+ * using the Shell sort algorithm, using the Knuth sequence
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
 void shell_sort(int *array, size_t size)
 {
 	int temp;
--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,88 +1,77 @@
 #include "sort.h"
 
-void swap(int *x, int *y);
-void quick_sort_recursion(int array[], int low, int high, size_t size);
-int partition(int array[], int low, int high, size_t size);
+/**
+ * swap - swaps 2 int
+ * @array: array to be sorted
+ * @size: array length
+ * @a: first int
+ * @b: second int
+ * Return: void
+ */
+void swap(int *array, size_t size, int *a, int *b)
+{
+	if (*a != *b)
+	{
+		*a = *a + *b;
+		*b = *a - *b;
+		*a = *a - *b;
+		print_array((const int *)array, size);
+	}
+}
+
+/**
+ * lomuto_partition - partitions the
+ * array using lomuto_partition
+ * @array: array to be sorted
+ * @size: array length
+ * @lo: the low index of the range
+ * @hi: the high index of the range
+ *
+ * Return: void
+ */
+size_t lomuto_partition(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	int i, j, pivot = array[hi];
+
+	for (i = j = lo; j < hi; j++)
+		if (array[j] < pivot)
+			swap(array, size, &array[j], &array[i++]);
+	swap(array, size, &array[i], &array[hi]);
+
+	return (i);
+}
+
+/**
+ * quicksort - sort using Lomuto partitioning scheme
+ * @array: array to be sorted
+ * @size: the array length
+ * @lo: the low index of the sort range
+ * @hi: the high index of the sort range
+ *
+ * Return: void
+ */
+void quicksort(int *array, size_t size, ssize_t lo, ssize_t hi)
+{
+	if (lo < hi)
+	{
+		size_t p = lomuto_partition(array, size, lo, hi);
+
+		quicksort(array, size, lo, p - 1);
+		quicksort(array, size, p + 1, hi);
+	}
+}
 
 /**
  * quick_sort - sorts an array of integers in
  * ascending order using the Quick sort algorithm
  * @array: array to be sorted
- * @size: size of the array to be sorted
+ * @size: the size of the array
+ *
  * Return: void
  */
 void quick_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
+	if (!array || !size)
 		return;
-
-	quick_sort_recursion(array, 0, (size - 1), size);
-
-}
-
-
-/**
- * swap - swap two in n an array
- * @x: first int to swap
- * @y: second int to swap
- * Return: void
- */
-void swap(int *x, int *y)
-{
-	int temp;
-
-	temp = *x;
-	*x = *y;
-	*y = temp;
-}
-
-/**
- * quick_sort_recursion - sort the array
- * @array: array to be sorted
- * @low: lower bound of the array part to be sorted
- * @high: higher bound of the array to be sorted
- * @size: size of the array
- * Return: void
- */
-void quick_sort_recursion(int array[], int low, int high, size_t size)
-{
-	int pivot;
-
-	if (low < high)
-	{
-
-	pivot = partition(array, low, high, size);
-	quick_sort_recursion(array, low, pivot - 1, size);
-	quick_sort_recursion(array, pivot + 1, high, size);
-	}
-
-}
-
-/**
- * partition - partition the array and swap base on partition
- * @array: array to be partitioned
- * @low: lower bound of the part of array to partition
- * @high: upper bound of array to be paritioned
- * @size: size of the array to be sorted
- * Return: the parttion index
- */
-int partition(int array[], int low, int high, size_t size)
-{
-	int j;
-	int pivot_value = array[high];
-	int i = low;
-
-	for (j = low; j < high; j++)
-	{
-		if (array[j] < pivot_value)
-		{
-			swap(&array[i], &array[j]);
-			i++;
-		
-		}
-	}
-	swap(&array[i], &array[high]);
-	print_array(array, size);
-
-	return (i);
+	quicksort(array, size, 0, size - 1);
 }
